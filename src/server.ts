dfdf91da6c4054763d413a46488aacec2b0557b9
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2025-03-06 03:31:43 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import app from './app';
+import logger from './utils/logger/logger';
+import dbConfig from './config/db-config';
+dotenv.config();
+const port = process.env.PORT || 3000;
+app.listen(port, async () => {
+    await dbConfig();
+    logger.info(`Server is running on port ${port}`);
+})
