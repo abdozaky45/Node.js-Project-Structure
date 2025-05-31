8a1275facbff94be4c71f840e4ef5ed3b745adf0
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2025-05-31 03:05:21 +0300

    Add user management functionality with user creation, repository, and service layers

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+import userRouter from './routers/user/user-router';
+import RouterEnum from './utils/routers';
+app.use(`/${RouterEnum.User}`, userRouter);
 app.get('/', (req, res) => {
     res.send('Node.js and TypeScript API');
 });
